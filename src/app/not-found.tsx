597c2d6093332fd989e7f642151519f10cfc5da6
feenko <147188774+feenko@users.noreply.github.com>
2024-10-09 22:04:01 +0200

    feat: initial 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,25 @@
+import { Button } from '~/components/ui/button';
+import Link from 'next/link';
+
+export default function NotFound() {
+  return (
+    <div className="mt-16 md:mt-32 flex h-full w-full items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-4xl font-semibold">Page not found</h1>
+        <p className="mt-2 text-muted-foreground">The page you are looking for does not exist.</p>
+
+        <div className="flex flex-col items-center justify-center">
+          <Button variant="default" asChild className="mt-4">
+            <Link href="/">Go Home</Link>
+          </Button>
+
+          <Button variant="link" asChild className="mt-2">
+            <a href="https://github.com/meteor-discord/" target="_blank" rel="noopener noreferrer">
+              Having issues? Contact us.
+            </a>
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
